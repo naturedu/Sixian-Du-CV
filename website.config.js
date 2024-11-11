8b075f0366c1bba10f0ba5a1648451d45cf9aa57
naturedu <dusixian16@163.com>
2024-11-11 11:17:13 +0800

    Initial commit

---
+++ website.config.js
@@ -0,0 +1,41 @@
+export const personalInfo = {
+  name: 'Hiroshi Ishii',
+  profilePicture: '/profile.jpg', //optional
+  role: 'Professor',
+  university: 'MIT Media Lab',
+  universityWebsite: 'https://www.zju.edu.cn/',
+  socialMedia: [
+    { name: 'Email', url: 'https://linkedin.com' },
+    { name: 'Twitter', url: 'https://twitter.com/anxndsgn' },
+    {
+      name: 'GitHub',
+      url: 'https://github.com/anxndsgn/academic-homepage-template',
+    },
+    { name: 'LinkedIn', url: 'https://linkedin.com' },
+    { name: 'ORCID', url: 'https://orcid' },
+    { name: 'Google Scholar', url: 'https://scholar.google.com' },
+  ],
+};
+
+export const websiteInfo = {
+  title: personalInfo.name,
+  description: 'HCI researcher',
+  // teaserImage: "/teaser.jpg",
+};
+
+export const navigations = [
+  { name: 'Projects', route: '/projects' },
+  { name: 'Publications', route: '/publications' },
+  // { name: "News", route: "/news" },
+  // { name: 'About', route: '/about' },
+  { name: 'CV', route: '/cv.pdf' },
+];
+
+export const homepageSection = {
+  AboutSection: true,
+  NewsSection: true,
+  // SelectedPublicationsSection: true,
+  // ProjectSection: true,
+};
+
+export const fontStyle = 'sans'; // "sans" | "serif" | "mono"
